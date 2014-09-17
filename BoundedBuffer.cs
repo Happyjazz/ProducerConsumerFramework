a815e84f1041f4c828ffad91c51b4f8c45be9562
Martin <martin@happyjazz.eu>
2014-09-17 13:48:30 +0200

    1st draft - BoundedBuffer - Den rigtigt

--- BoundedBuffer.cs
+++ BoundedBuffer.cs
@@ -8,19 +8,32 @@ namespace ProducerConsumerFramework
 {
     class BoundedBuffer
     {
+        private Queue<int> _queue;
+        private int _bufferSize;
+
         public BoundedBuffer(int bufferSize)
         {
-            
+            _bufferSize = bufferSize;
+            _queue = new Queue<int>();
         }
 
-        public void Put()
+        public bool IsFull()
         {
-            
+            bool result = _queue.Count >= _bufferSize;
+            return result;
         }
 
-        public void Take()
+        public void Put(int inputValue)
         {
-            
+            if (IsFull())
+            {
+                _queue.Enqueue(inputValue);
+            }
+        }
+
+        public int Take()
+        {
+            return _queue.Dequeue();
         }
     }
 }
