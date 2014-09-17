f14b5125add6a3c0b9ae1df31a8ecf9888c55c70
Martin <martin@happyjazz.eu>
2014-09-17 13:14:24 +0200

    Initial Commit

---
+++ BoundedBuffer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProducerConsumerFramework
+{
+    class BoundedBuffer
+    {
+        public BoundedBuffer(int bufferSize)
+        {
+            
+        }
+
+        public void Put()
+        {
+            
+        }
+
+        public void Take()
+        {
+            
+        }
+    }
+}
